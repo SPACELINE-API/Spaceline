f6d391df4c10f068db817fb8b387a46430c74746
rafaelapenafiel <rafaela.diniz@fatec.sp.gov.br>
2024-11-21 15:33:55 -0300

    footer

--- Páginas API/search.js
+++ Páginas API/search.js
@@ -1,6 +1,6 @@
 const pages = [
-    { title: "Introdução", url: "intro.html", content: [{ id: "h2", text: "O que é a métodogia ágil." },] },
-    { title: "Scrum", url: "scrum.html", content: [{ id: "margin1", text: "O que é o método scrum?" }, { id: "margin2", text: "Quais os benefícios de usar o scrum?" }] },
+    { title: "Introdução", url: "intro.html", content: [{ id: "h2", text: "O que é a metodogia ágil." },] },
+    { title: "Scrum", url: "scrum.html", content: [{ id: "margin1", text: "O que é o metodo scrum?" }, { id: "margin2", text: "Quais os benefícios de usar o scrum?" }] },
     { title: "Artefatos", url: "artefatos.html", content: [{ id: "margin3", text: "Backlog do produto." }, { id: "margin4", text: "Burndown" },{ id: "margin5", text: "User Story" },{ id: "margin6", text: "DOR E DOD" },{ id: "margin7", text: "MVP" }] },
     { title: "Scrum Master", url: "master.html", content:  [{ id: "w", text: "O que é?" },] },
     { title: "Product Owner", url: "po.html", content: [{ id: "h", text: "O que é?" },] },
