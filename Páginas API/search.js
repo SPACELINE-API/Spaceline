e18e5064fcd6a9ebceaf8858d15c6b11e5d4b0bb
Yasmin Padilha <yasmincp757@gmail.com>
2024-11-22 02:25:21 -0300

    pesquisa

--- Páginas API/search.js
+++ Páginas API/search.js
@@ -1,10 +1,10 @@
 const pages = [
-    { title: "Introdução", url: "intro.html", content: [{ id: "h2", text: "O que é a métodogia ágil." },] },
-    { title: "Scrum", url: "scrum.html", content: [{ id: "margin1", text: "O que é o método scrum?" }, { id: "margin2", text: "Quais os benefícios de usar o scrum?" }] },
-    { title: "Artefatos", url: "artefatos.html", content: [{ id: "margin3", text: "Backlog do produto." }, { id: "margin4", text: "Burndown" },{ id: "margin5", text: "User Story" },{ id: "margin6", text: "DOR E DOD" },{ id: "margin7", text: "MVP" }] },
-    { title: "Scrum Master", url: "master.html", content:  [{ id: "w", text: "O que é?" },] },
-    { title: "Product Owner", url: "po.html", content: [{ id: "h", text: "O que é?" },] },
-    { title: "Soft Skills", url: "softskills.html", content: [{ id: "margin8", text: "O que são?" },] },
+    { title: "Introdução", url: "intro.html", content: [{ id: "metodoagil", text: "O que é a métodogia ágil." },] },
+    { title: "Scrum", url: "scrum.html", content: [{ id: "definicao", text: "O que é o método scrum?" }, { id: "margin2", text: "Quais os benefícios de usar o scrum?" }] },
+    { title: "Artefatos", url: "artefatos.html", content: [{ id: "bkproduto", text: "Backlog do produto." }, { id: "graficobd", text: "Burndown" },{ id: "us", text: "User Story" },{ id: "dd", text: "DOR E DOD" },{ id: "mvproduto", text: "MVP" }] },
+    { title: "Scrum Master", url: "master.html", content:  [{ id: "smaster", text: "O que é?" },] },
+    { title: "Product Owner", url: "po.html", content: [{ id: "powner", text: "O que é?" },] },
+    { title: "Soft Skills", url: "softskills.html", content: [{ id: "sfk", text: "O que são?" },] },
 ];
 
 function searchSite() {
